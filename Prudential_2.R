dc51ab194d272b6bb7a57df91e240355219ff165
Deonarine <adeonari@r1-core.r1.aig.net>
2015-12-09 17:33:54 -0500

    binary encoding

--- Prudential_2.R
+++ Prudential_2.R
@@ -123,6 +123,58 @@ for(i in cat.table.3$var) {
   
 }
 
+#Parse the high cardinality columns manually
+
+#Employment_Info_2
+all.cat.vars %>% count(Employment_Info_2)
+
+col.names <- paste("Employment_Info_2", 1:6, sep='.')
+t.1 <- all.cat.vars %>%
+  select(Id, Employment_Info_2) %>%
+  mutate(bin = intToBin(Employment_Info_2)) %>%
+  rowwise() %>%
+  mutate(bin.split = paste0(strsplit(bin, split='')[[1]], collapse = ".")) %>%
+  separate(bin.split, col.names, convert=TRUE) %>%
+  select(-Employment_Info_2, -bin)
+
+#InsuredInfo_3
+all.cat.vars %>% count(InsuredInfo_3)
+
+col.names <- paste("InsuredInfo_3", 1:4, sep='.')
+t.2 <- all.cat.vars %>%
+  select(Id, InsuredInfo_3) %>%
+  mutate(bin = intToBin(InsuredInfo_3)) %>%
+  rowwise() %>%
+  mutate(bin.split = paste0(strsplit(bin, split='')[[1]], collapse = ".")) %>%
+  separate(bin.split, col.names, convert=TRUE) %>%
+  select(-InsuredInfo_3, -bin)
+
+#Product_Info_3
+all.cat.vars %>% count(Product_Info_3)
+
+col.names <- paste("Product_Info_3", 1:6, sep='.')
+t.3 <- all.cat.vars %>%
+  select(Id, Product_Info_3) %>%
+  mutate(bin = intToBin(Product_Info_3)) %>%
+  rowwise() %>%
+  mutate(bin.split = paste0(strsplit(bin, split='')[[1]], collapse = ".")) %>%
+  separate(bin.split, col.names, convert=TRUE) %>%
+  select(-Product_Info_3, -bin)
+
+#Product_Info_2
+Product_Info_2 <- all.cat.vars %>% count(Product_Info_2) %>%
+  mutate(Product_Info_2.recode = row_number())
+
+col.names <- paste("Product_Info_2", 1:5, sep='.')
+t.4 <- all.cat.vars %>%
+  select(Id, Product_Info_2) %>%
+  left_join(Product_Info_2) %>%
+  mutate(bin = intToBin(Product_Info_2.recode)) %>%
+  rowwise() %>%
+  mutate(bin.split = paste0(strsplit(bin, split='')[[1]], collapse = ".")) %>%
+  separate(bin.split, col.names, convert=TRUE) %>%
+  select(-Product_Info_2, -bin, -n, -Product_Info_2.recode)
+
 #Remove old variables
 columns.to.drop <- cat.table %>%
   filter(categories==2 | categories==3)
@@ -130,15 +182,23 @@ columns.to.drop <- columns.to.drop$var
 
 for(i in columns.to.drop) {
   all.cat.vars <- all.cat.vars %>%
+    #deselect original column name
     select_(interp(~-(x), x=as.name(paste0(i))))
 }
+
+all.cat.vars <- all.cat.vars %>%
+  select(-Product_Info_2, -Product_Info_3, -InsuredInfo_3, -Employment_Info_2) %>%
+  left_join(t.1) %>%
+  left_join(t.2) %>%
+  left_join(t.3) %>%
+  left_join(t.4)
 
 ####build model
 train.data <- train.full %>%
   select(Id, Response,
          Product_Info_4, Ins_Age, Ht, Wt, BMI) %>%
-  left_join(all.cat.vars) %>%
-  select(-Product_Info_3, -Employment_Info_2, -Product_Info_2, -InsuredInfo_3)
+  left_join(all.cat.vars)
+  #select(-Product_Info_3, -Employment_Info_2, -Product_Info_2, -InsuredInfo_3)
 
 set.seed(100)
 
@@ -205,6 +265,9 @@ table(oos.test.y)/length(oos.test.y)
 dtrain <- xgb.DMatrix(data = train.data.model, label = train.data.model.y)
 dtest <- xgb.DMatrix(data = train.data.watch, label = train.data.watch.y)
 
+dtrain.all <- xgb.DMatrix(data = rbind(train.data.model, train.data.watch), 
+                          label = rbind(train.data.model.y, train.data.watch.y))
+
 param.1 <- list("objective" = "multi:softmax",
                 #"eval_metric" = "mlogloss",
                 "num_class" = 8,
@@ -229,20 +292,42 @@ bst.2 <- xgb.train(param = param.2, data = dtrain,
                    nrounds = nround, verbose = 1,
                    eval.metric = "merror", eval.metric = "mlogloss")
 
-bst.3 <- xgb.train(data=dtrain, max.depth=10, eta=.1, nround=50, 
+bst.3 <- xgb.train(data=dtrain, max.depth=20, eta=.5, nround=50, 
                    watchlist=watchlist, num_class=8,
                    eval.metric = "merror", eval.metric = "mlogloss",
                    nthread = 8, objective = "multi:softmax")
 
+reg.1 <- xgb.train(data=dtrain, max.depth=10, eta=.05, nround=100, 
+                   watchlist=watchlist,
+                   eval.metric = "rmse",
+                   nthread = 8, objective = "reg:linear")
+reg.2 <- xgb.train(data=dtrain, max.depth=10, nround=500, 
+                   booster = "gblinear",
+                   watchlist=watchlist,
+                   eval.metric = "rmse",
+                   nthread = 8, objective = "reg:linear")
+
+param.4 <- list("objective" = "multi:softmax",
+                "num_class" = 8,
+                "max_depth" = 20,
+                "eta" = .05)
+bst.4 <- xgb.train(param = param.4, data = dtrain.all, 
+                   #watchlist = watchlist,
+                   nrounds = 100, verbose = 2,
+                   feval = evalkappa.gbm, nthread = 8)
+
 pred.1 <- predict(bst.1, oos.test)
 pred.2 <- predict(bst.2, oos.test)
 pred.3 <- predict(bst.3, oos.test)
+pred.4 <- predict(bst.4, oos.test)
 
 table(pred.1)
 table(pred.3)
 
 ggplot(as.data.frame(pred.1)) + geom_histogram(aes(pred.1), binwidth=.5)
 ggplot(as.data.frame(pred.2)) + geom_histogram(aes(pred.2), binwidth=.5)
+
+ggplot(as.data.frame(pred.4)) + geom_histogram(aes(pred.4), binwidth=.5)
 
 #getinfo(dtrain, "label") + 1
 
@@ -264,4 +349,16 @@ head(pred.1)
 evalkappa(oos.test.y, pred.1)
 evalkappa(oos.test.y, pred.2)
 evalkappa(oos.test.y, pred.3)
+evalkappa(oos.test.y, pred.4)
 
+#Reg
+pred.reg <- predict(reg.1, oos.test)
+pred.reg[pred.reg<0] <- 0
+pred.reg[pred.reg>7] <- 7
+evalkappa(oos.test.y, pred.reg)
+
+#boosted
+pred.reg <- predict(reg.2, oos.test)
+pred.reg[pred.reg<0] <- 0
+pred.reg[pred.reg>7] <- 7
+evalkappa(oos.test.y, pred.reg)
